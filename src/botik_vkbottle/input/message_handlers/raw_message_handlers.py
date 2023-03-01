efbf9fe6583a22a97e601b7e4214183b2109c043
dasstyx <dasstyx@gmail.com>
2023-03-02 02:17:03 +0300

    add photos handling

--- src/botik_vkbottle/input/message_handlers/raw_message_handlers.py
+++ src/botik_vkbottle/input/message_handlers/raw_message_handlers.py
@@ -1,11 +1,14 @@
-from vkbottle.dispatch.rules.base import GeoRule
+from vkbottle.dispatch.rules.base import GeoRule, AttachmentTypeRule
 
 from botik.input.message_handlers.events import events
 from botik.input.message_handlers.raw_message_handlers import RawMessageHandlers
 
+from botik_vkbottle.api.attachment_handlers.photo_attachment_handler import PhotoAttachmentHandler
+
 
 class RawMessageHandlers(RawMessageHandlers):
     def _initialize_handlers(self, bot):
+        bot.on.private_message(AttachmentTypeRule("photo"))(self.photo_reply)
         bot.on.private_message()(self.message_reply)
         bot.on.private_message(GeoRule())(self.location_reply)
         bot.on.private_message(text=["Начать"])(self.start_reply)
@@ -24,6 +27,12 @@ class RawMessageHandlers(RawMessageHandlers):
         user = await self._get_user_from_id(user_id)
 
         await self.user_input.forward_inline_button(user, data)
+
+    async def photo_reply(self, message):
+        user = await self._get_user_from_message(message)
+        handler = PhotoAttachmentHandler()
+        photo = await handler.from_server(message)
+        await events.got_attachment(user, photo)
 
     async def location_reply(self, message):
         user = await self._get_user_from_message(message)
